996be28ce7533c2139dad7ab3d5482d737b55b63
HELIOPOTELICKI <helio.p2000@hotmail.com>
2020-06-17 00:02:34 -0300

    Finalizado

--- Trabalho Mapa de Dispersao/MapaDispersao.java
+++ Trabalho Mapa de Dispersao/MapaDispersao.java
@@ -21,36 +21,48 @@ public class MapaDispersao<K, T> {
 
 		quantidade++;
 		while (!isprimo(quantidade)) {
+
 			quantidade++;
+
 		}
+
 		return quantidade;
 	}
 
 	private static boolean isprimo(int numero) {
 		for (int j = 2; j < numero; j++) {
+
 			if (numero % j == 0)
 				return false;
+
 		}
+
 		return true;
 	}
 
 	public int calcularHash(K chave) {
-		int hashCode = Math.abs(chave.hashCode());
-		int hash = hashCode % getProximoPrimo(quantidade);
+		int hash = Math.abs(chave.hashCode()) % getProximoPrimo(quantidade);
 		return hash;
 	}
 
 	public boolean inserir(K chave, T valor) {
+
 		if (this.buscar(chave) != null) {
 			throw new IllegalArgumentException("Item ja cadastrado.");
 		}
+
 		int indice = calcularHash(chave);
+
 		if (tabela[indice] == null) {
+
 			ListaEncadeada<T> lista = new ListaEncadeada<T>();
 			lista.insere(valor);
 			tabela[indice] = lista;
+
 		} else {
+
 			tabela[indice].insere(valor);
+
 		}
 		tamanhoAtual++;
 		return true;
@@ -58,28 +70,35 @@ public class MapaDispersao<K, T> {
 
 	@SuppressWarnings("unchecked")
 	public T remover(K chave) {
+
 		int indice = calcularHash(chave);
-		if (tabela[indice] == null) {
+
+		if (tabela[indice] == null)
 			return null;
-		}
+
 		ListaEncadeada<T> itens = tabela[indice];
 		int posicaoLista = itens.localiza((T) chave);
 		T retorno = itens.busca(posicaoLista);
-		if (retorno == null) {
+
+		if (retorno == null)
 			return null;
-		}
+
 		itens.retira(posicaoLista);
 		tamanhoAtual--;
+
 		return retorno;
 	}
 
 	@SuppressWarnings("unchecked")
 	public T buscar(K chave) {
+
 		int indice = calcularHash(chave);
-		if (tabela[indice] == null) {
+
+		if (tabela[indice] == null)
 			return null;
-		}
+
 		ListaEncadeada<T> itens = tabela[indice];
+
 		return itens.busca(itens.localiza((T) chave));
 	}
 
